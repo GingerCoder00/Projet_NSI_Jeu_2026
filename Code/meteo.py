f672940ac90fc638ea684e82df1c144f99be4fb2
GingerCoder00 <arthur.leguludec@gmail.com>
2026-02-17 20:10:10 +0100

    Ajout d'un fichier meteo.py pour gérer la météo sur la grille

---
+++ Code/meteo.py
@@ -0,0 +1,31 @@
+import pygame
+import os
+
+class Meteo:
+    def __init__(self, zone_grille_x, zone_grille_y):
+
+        self.zone_x = zone_grille_x
+        self.zone_y = zone_grille_y
+
+        self.last_frame = pygame.time.get_ticks()
+
+        self.RAIN_PATH = os.path.join(self.BASE_DIR, "sprite", "sprite_effet_pluie", "sprite_pluie_")
+        self.sprite_pluie = [pygame.image.load(f"{self.RAIN_PATH}{str(i).zfill(2)}.png").convert() for i in range(23)] # Importation des 23 frames
+        self.sprite_pluie = [pygame.transform.scale(elt, (self.zone_L, self.zone_l)) for elt in self.sprite_pluie] # Convertion des frames pour la grille
+        self.pluie_frame = 0      
+
+    def pluie(self):
+        '''
+        Cette méthode gère la pluie
+        '''
+        now = pygame.time.get_ticks()
+        rain_delay = 50 # Delai en ms
+
+        if now - self.last_rain_update >= rain_delay:
+            self.pluie_frame = (self.pluie_frame + 1) % len(self.sprite_pluie)
+            self.last_rain_update = now
+
+        image = self.sprite_pluie[self.pluie_frame]
+        image.set_alpha(155)  # Change l'opacité ici
+
+        self.screen.blit(image, (self.zone_x, self.zone_y))
